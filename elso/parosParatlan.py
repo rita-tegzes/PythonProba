31ddf584991f52dd522ec372bc6523929499e8c7
Rita Tegzes <rita.tegzes@gmail.com>
2015-03-07 14:33:00 +0100

    parosParatlan

---
+++ elso/parosParatlan.py
@@ -0,0 +1,9 @@
+__author__ = 'Rita'
+
+# maradekos oszatas "a % 2" ez megmondja, hogy a szam 2-vel osztva, mennyi maradékot ad.
+a = 13
+
+if (a % 2) == 0:
+    print("paros")
+else:
+    print("paratlan")
