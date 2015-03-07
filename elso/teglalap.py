323b9d6c2fa3d14bbdddc3071d10be6523313029
Rita Tegzes <rita.tegzes@gmail.com>
2015-03-07 15:02:10 +0100

    piti

---
+++ elso/teglalap.py
@@ -0,0 +1,8 @@
+__author__ = 'Rita'
+# teglalap terulet
+a = 2
+b = 3
+terulet = a * b
+kerulet = (a + b ) * 2
+print("Az ", a, b, "oldalú téglalap területe:", terulet)
+print("Az ", a, b, "oldalú téglalap kerülete:", kerulet)
