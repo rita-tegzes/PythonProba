9d4bb92646ac5a944f9683582f835c0bd3fadf84
Rita Tegzes <rita.tegzes@gmail.com>
2015-03-07 14:20:56 +0100

    kitalalos

---
+++ elso/kitalalos.py
@@ -0,0 +1,12 @@
+__author__ = 'Rita'
+
+szam = 212
+proba =212
+
+if proba == szam:
+    print("kitalaltad")
+else:
+    if proba < szam:
+        print("ennel nagyobbra gondoltam")
+    else:
+        print("ennel kissebre gondoltam")
